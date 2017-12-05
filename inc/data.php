9a874a7e2a16688fa303291be9e0141941a555f3
jeremym96 <34288555+jeremym96@users.noreply.github.com>
2017-12-05 18:53:40 -0500

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,129 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "I Live In Your Basement!",
+	"img" => "img/media/design_patterns.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 1997,
+    "category" => "Books",
+    "authors" => [
+        "R.L. Stine"
+	],
+    "publisher" => "Scholastic",
+    "isbn" => '978-0590399869'
+];
+$catalog[102] = [
+    "title" => "Night Of The Living Dummy",
+    "img" => "img/media/clean_code.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 1995,
+    "category" => "Books",
+    "authors" => [
+        "R.L. Stine"
+    ],
+    "publisher" => "Scholastic",
+    "isbn" => '978-0590553667'
+];
+$catalog[104] = [
+    "title" => "Monster Blood",
+    "img" => "img/media/clean_coder.jpg",
+    "genre" => "Horror",
+    "format" => "Paperback",
+    "year" => 1992,
+    "category" => "Books",
+    "authors" => [
+        "R.L. Stine"
+    ],
+    "publisher" => "Scholastic",
+    "isbn" => '978-0590453677'
+];
+//Movies
+$catalog[201] = [
+    "title" => "The Grudge 3",
+    "img" => "img/media/forest_gump.jpg",
+    "genre" => "Horror",
+    "format" => "DVD",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Toby Wilkins",
+    "writers" => [
+        "Brad Keene"
+    ],
+    "stars" => [
+        "Johanna Braddy",
+        "Gil McKinney",
+        "Shawnee Smith"
+    ]
+];
+$catalog[202] = [
+    "title" => "Sanky Panky 2",
+    "img" => "img/media/office_space.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2013,
+    "category" => "Movies",
+    "director" => "José Enrique Pintor",
+    "writers" => [
+        "Shandy Cuesta"
+    ],
+    "stars" => [
+        "Fausto Mata",
+        "Tony Pascual",
+        "Aquiles Correa",
+        "Alina Vargas",
+        "Zdenka Kalina",
+        "Massimo Borghetti"
+    ]
+];
+$catalog[203] = [
+    "title" => "Iron man 2",
+    "img" => "img/media/lotr.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2010,
+    "category" => "Movies",
+    "director" => "Jon Favreau",
+    "writers" => [
+        "Jon Favreau"
+    ],
+    "stars" => [
+        "Robert Downey Jr.",
+        "Gwyneth Paltrow",
+        "Don Cheadle",
+        "Scarlett Johansson",
+        "Sam Rockwell",
+        "Mickey Rourke",
+	"Samuel L. Jackson"
+    ]
+];
+//Music
+$catalog[302] = [
+    "title" => "Humanz",
+    "img" => "img/media/elvis_presley.jpg",
+    "genre" => "Electronic",
+    "format" => "Digital",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Gorillaz"
+];
+$catalog[303] = [
+    "title" => "Encore",
+    "img" => "img/media/garth_brooks.jpg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2004,
+    "category" => "Music",
+    "artist" => "Eminem"
+];
+$catalog[304] = [
+    "title" => "Perception",
+    "img" => "img/media/nat_king_cole.jpg",
+    "genre" => "Hip Hop",
+    "format" => "Digital",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "NF"
+];
